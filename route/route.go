ca98154a98e09d543ab6d5f2b8e05232ca20268f
takeivahara <31751935+takeivahara@users.noreply.github.com>
2017-09-07 20:15:11 -0700

    Add files via upload

---
+++ route/route.go
@@ -0,0 +1,75 @@
+package route
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"projetoPOC/controller"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+		Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"HelloName",
+		"GET",
+		"/hello/{name}",
+		controller.HelloName,
+	},
+
+	Route{
+		"Operadoras",
+		"GET",
+		"/operadoras",
+		controller.Operadoras,
+	},
+
+	Route{
+		"Users",
+		"GET",
+		"/users",
+		controller.Users,
+	},
+
+	Route{
+		"Users",
+		"OPTIONS",
+		"/users",
+		controller.Users,
+	},
+
+	Route{
+		"Contato",
+		"GET",
+		"/contato",
+		controller.Contato,
+	},
+
+	Route{
+		"BuscarTodasPessoas",
+		"GET",
+		"/buscarTodasPessoas",
+		controller.BuscarTodasPessoa,
+	},
+}
+
